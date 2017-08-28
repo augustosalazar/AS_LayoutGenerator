02c2cf98ca8a331320ac9a961c9ec1888c7c0bff
augustosalazar <augustosalazar@uninorte.edu.co>
2017-08-28 18:57:28 -0500

    Tres diferentes metodos para generar layouts

--- app/src/main/java/co/edu/uninorte/layoutgenerator/MainActivity.java
+++ app/src/main/java/co/edu/uninorte/layoutgenerator/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                generateLayout2();
+                generateLayout3();
             }
         });
     }
@@ -66,13 +67,14 @@ public class MainActivity extends AppCompatActivity {
         newLayout.setOrientation(LinearLayout.VERTICAL);
         TextView tv = new TextView(this);
         EditText et = new EditText(this);
+        et.setId(count);
         Button bt = new Button(this);
         bt.setText("Button "+count);
-
+        bt.setTag(count);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int id = view.getId();
+                int id = (int) view.getTag();
                 EditText e = (EditText) findViewById(id);
                 Log.d("LayoutGeneratorLog","El boton "+e.getText());
             }
@@ -84,6 +86,29 @@ public class MainActivity extends AppCompatActivity {
         newLayout.addView(bt);
         main.addView(newLayout);
 
+        count++;
+    }
+
+    private void generateLayout3() {
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View v = inflater.inflate(R.layout.inside, newLayout, false);
+
+        Button button = (Button) v.findViewById(R.id.elBoton);
+        final EditText et = (EditText) v.findViewById(R.id.elEditText);
+        button.setText("Button "+count);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("LayoutGeneratorLog","El boton "+et.getText());
+            }
+        });
+
+
+        newLayout.addView(v);
+
+        main.addView(newLayout);
         count++;
     }
 }
