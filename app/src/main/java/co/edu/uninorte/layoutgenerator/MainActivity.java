cb5bba165b92d997e9a79d47e39babafb3a1062c
Augusto <augustosalazar@uninorte.edu.co>
2017-08-28 17:21:36 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/uninorte/layoutgenerator/MainActivity.java
@@ -0,0 +1,89 @@
+package co.edu.uninorte.layoutgenerator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout main;
+    int count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        main = (LinearLayout) findViewById(R.id.mainLayout);
+        count = 1;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateLayout2();
+            }
+        });
+    }
+
+    private void generateLayout() {
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView tv = new TextView(this);
+        EditText et = new EditText(this);
+        Button bt = new Button(this);
+        bt.setText("Button "+count);
+        bt.setTag(count);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("LayoutGeneratorLog","El boton "+view.getTag());
+            }
+        });
+        tv.setText("el tv");
+
+        newLayout.addView(tv);
+        newLayout.addView(et);
+        newLayout.addView(bt);
+        main.addView(newLayout);
+        count++;
+    }
+
+
+    private void generateLayout2() {
+        LinearLayout newLayout = new LinearLayout(this);
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView tv = new TextView(this);
+        EditText et = new EditText(this);
+        Button bt = new Button(this);
+        bt.setText("Button "+count);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = view.getId();
+                EditText e = (EditText) findViewById(id);
+                Log.d("LayoutGeneratorLog","El boton "+e.getText());
+            }
+        });
+        tv.setText("el tv");
+
+        newLayout.addView(tv);
+        newLayout.addView(et);
+        newLayout.addView(bt);
+        main.addView(newLayout);
+
+        count++;
+    }
+}
---
+++ app/src/main/java/co/edu/uninorte/layoutgenerator/MyButton.java
@@ -0,0 +1,19 @@
+package co.edu.uninorte.layoutgenerator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by Usuario on 28/08/2017.
+ */
+
+public class MyButton extends android.support.v7.widget.AppCompatButton {
+
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+}
